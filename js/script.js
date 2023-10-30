00f5faba3068761b87b0af1837f0e1d39d3e7b2e
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-10-30 10:06:00 +0100

    add: function generateAnyGrid to semplify content

--- js/script.js
+++ js/script.js
@@ -7,44 +7,66 @@ const selectElem = document.getElementById("difficulty");
 console.log(selectElem);
 
 //logic
-//Normal grid 10x10
 playElem.addEventListener("click", function () {
   switch (selectElem.value) {
+    //Easy grid 10x10
     case "easy":
       gridElem.innerHTML = "";
-      for (let i = 1; i <= 100; i++) {
+      generateAnyGrid(100, "easy");
+      /* for (let i = 1; i <= 100; i++) {
         const cell = generateGridCell(i);
         cell.addEventListener("click", heandleCell);
         gridElem.append(cell);
-      }
+      } */
 
       break;
-
+    // Medium grid 9x9
     case "medium":
       gridElem.innerHTML = "";
-      for (let i = 1; i <= 81; i++) {
+      generateAnyGrid(81, "medium");
+      /*  for (let i = 1; i <= 81; i++) {
         const cell = generateGridCell(i);
         cell.addEventListener("click", heandleCell);
         cell.classList.add("medium");
         gridElem.append(cell);
-      }
+      } */
 
       break;
-
+    //Hard grid
     case "hard":
       gridElem.innerHTML = "";
-      for (let i = 1; i <= 49; i++) {
+      generateAnyGrid(49, "hard");
+      /* for (let i = 1; i <= 49; i++) {
         const cell = generateGridCell(i);
         cell.addEventListener("click", heandleCell);
         cell.classList.add("hard");
         gridElem.append(cell);
-      }
+      } */
 
       break;
   }
 });
 
 //FUNCTION
+
+/* ************************* */
+
+/**
+ * Description: Create a grif with any number of cell inside
+ * @param {any} gridLenght, the number of cell you want.
+ * @param {any} difficulty, the class to add to any cell.
+ * @returns {any} Html element, grid with cell.
+ */
+function generateAnyGrid(gridLenght, difficulty) {
+  for (let i = 1; i <= gridLenght; i++) {
+    const cell = generateGridCell(i);
+    cell.addEventListener("click", heandleCell);
+    cell.classList.add(difficulty);
+    gridElem.append(cell);
+  }
+}
+
+/* ************************* */
 
 /**
  * Description create one cell with a number inside
@@ -58,7 +80,7 @@ function generateGridCell(innerNumber) {
   return newcell;
 }
 
-/* **** */
+/* ************************* */
 
 /**
  * Description add lightgreen class
